aadad1e19836b1799e5785f306dbebe31b38e158
tjforys <tomekk925@gmail.com>
2024-04-12 21:24:04 +0200

    added function for teleporting to shard boss

--- gem_trader.py
+++ gem_trader.py
@@ -15,15 +15,28 @@ def is_auto_refresh_text_present():
 
 
 def teleport_to_witch_and_back():
-    witch_area_x, witch_area_y = 1770, 1036
-    inner_pyramid_x, inner_pyramid_y = 1770, 750
+    witch_area_x, witch_area_y = 1770, 465
+    inner_pyramid_x, inner_pyramid_y = 1770, 329
     pyautogui.press("v")
     move_click(witch_area_x, witch_area_y)
-    time.sleep(5)
+    time.sleep(6)
     move_click(inner_pyramid_x, inner_pyramid_y)
     pyautogui.press("y")
-
-
+    
+def teleport_to_shard_boss_and_back():
+    time.sleep(0.5)
+    inner_pyramid_x, inner_pyramid_y = 1770, 329
+    boss_x, boss_y = 1770, 767
+    pyautogui.press("v")
+    move_click(boss_x, boss_y)
+    time.sleep(1)
+    pyautogui.press("v")
+    for i in range(5):
+        move_click(1236, 740)
+        time.sleep(2)
+    pyautogui.press("v")
+    move_click(inner_pyramid_x, inner_pyramid_y)
+    pyautogui.press("y")
 cancel_coords = 1792 
 gem_x_coord = 1252
 start_button_x_coord = 2090
@@ -35,29 +48,33 @@ gui_button_y = 1160
 cancel_color = (221, 26, 30)
 gem_color = (255, 0, 68)
 cheese_color = (244, 180, 27)
-beer_color = (173, 193, 188)
+beer_color = (174, 195, 190)
 borb_color = (206, 245, 135)
 
-current_material = gem_color
+current_material_list = [gem_color]
 start_game = False
 
 while True:
     if keyboard.is_pressed("o"):
         start_game = True
         teleport_to_witch_and_back()
-        start_time=time.time()
-
+        witch_start_time=time.time()
+        teleport_to_shard_boss_and_back()
+        shard_boss_start_time = time.time()
     if start_game:
-        if time.time() - start_time >= 165:
+        if time.time() - witch_start_time >= 150:
             teleport_to_witch_and_back()
-            start_time = time.time()
+            witch_start_time = time.time()
 
+        if time.time() - shard_boss_start_time >= 180:
+            teleport_to_shard_boss_and_back()
+            shard_boss_start_time = time.time()
         if is_auto_refresh_text_present():
             pyautogui.scroll(-1)
 
         current_screen = ImageGrab.grab().load()
         for y_coord in cancel_coord_list:
-            if current_screen[gem_x_coord, y_coord] == current_material:
+            if current_screen[gem_x_coord, y_coord] in current_material_list:
                 if current_screen[cancel_coords, y_coord] != cancel_color:
                     move_click(start_button_x_coord, y_coord)
 
